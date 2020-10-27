720e6e3e106c47852bde808347b1f56a54a94468
mfathur <mfathur17@gmail.com>
2020-10-27 15:14:10 +0700

    add toolbar menu

--- app/src/main/java/com/mfathurz/moviecatalogue/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mfathurz/moviecatalogue/ui/home/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.mfathurz.moviecatalogue.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import com.mfathurz.moviecatalogue.R
@@ -18,11 +22,29 @@ class HomeActivity : AppCompatActivity() {
         viewPager.adapter = HomeViewPagerAdapter(this)
 
         TabLayoutMediator(tabLayout, viewPager) { currentTab, currentPosition ->
-            currentTab.text = when (currentPosition){
+            currentTab.text = when (currentPosition) {
                 MOVIE_FRAGMENT -> getString(R.string.movie_fragment_title)
                 TV_SHOW_FRAGMENT -> getString(R.string.tv_show_fragment_title)
                 else -> getString(R.string.viewpager_error)
             }
         }.attach()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_setting -> {
+                val intent = Intent(Settings.ACTION_SETTINGS)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
