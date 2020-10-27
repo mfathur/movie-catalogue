8e1544ecbb774ea3505958f96e16e12e63640222
mfathur <mfathur17@gmail.com>
2020-10-28 00:08:49 +0700

    layout item recycler & detail activity completed

--- app/src/main/java/com/mfathurz/moviecatalogue/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/mfathurz/moviecatalogue/ui/detail/DetailActivity.kt
@@ -1,27 +1,29 @@
 package com.mfathurz.moviecatalogue.ui.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.mfathurz.moviecatalogue.R
 import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var viewModel: DetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        detailToolbar.title = "Detail"
-        setSupportActionBar(detailToolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         viewModel = ViewModelProvider(this)[DetailViewModel::class.java]
+
+        btnBackToHome.setOnClickListener(this)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnBackToHome -> {
+                onBackPressed()
+            }
+        }
     }
 }
